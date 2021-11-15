b9425b1c86f5222b02ccab3ffcc6d2e435190f82
Chris-eminence <emichris380@gmail.com>
2021-11-15 16:53:37 +0100

    Initial commit

--- app/src/main/java/com/example/kotlib/MainActivity.kt
+++ app/src/main/java/com/example/kotlib/MainActivity.kt
@@ -2,10 +2,27 @@ package com.example.kotlib
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import com.example.kotlib.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+
+    private lateinit var navCon : NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHost = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navCon = navHost.navController
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navCon.navigateUp() || super.onSupportNavigateUp()
+    }
+
+    override fun onBackPressed() {
     }
 }
--- app/src/main/java/com/example/kotlib/fragments/SplashScreenFragment.kt
+++ app/src/main/java/com/example/kotlib/fragments/SplashScreenFragment.kt
@@ -1,28 +1,40 @@
 package com.example.kotlib.fragments
 
 import android.os.Bundle
+import android.os.Handler
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
 import com.example.kotlib.R
+import com.example.kotlib.databinding.ActivityMainBinding.inflate
+import com.example.kotlib.databinding.FragmentSplashScreenBinding
+import com.example.kotlib.databinding.FragmentSplashScreenBinding.inflate
 
-class SplashScreenFragment : Fragment() {
-
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-
-        }
-    }
+class SplashScreenFragment : Fragment(){
+    private lateinit var _binding : FragmentSplashScreenBinding
+    private val binding get() = _binding
+    private lateinit var navCon : NavController
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_splash_screen, container, false)
+        _binding = FragmentSplashScreenBinding.inflate(inflater, container, false)
+        navCon = NavHostFragment.findNavController(this)
+
+
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        Handler().postDelayed({
+            navCon.navigate(R.id.action_splashScreenFragment_to_signUpFragment2)
+        }, 3000)
+    }
 }
+
+
